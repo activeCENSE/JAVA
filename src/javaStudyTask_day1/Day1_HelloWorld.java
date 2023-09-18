0cff9c48e3c61073cc46c4ffa5661130c9692344
jsum01 <djadjawltjr@gmail.com>
2023-09-18 23:52:40 +0900

    안녕세상

--- src/javaStudyTask_day1/Day1_HelloWorld.java
+++ src/javaStudyTask_day1/Day1_HelloWorld.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 public class Day1_HelloWorld {
 
     public static void main(String[] args) {
+    	//환영문구 출력
+    	System.out.println("HelloWorld");
+    	
     	Scanner scanner = new Scanner(System.in);
     	System.out.println("입력할 문구는 ?");
     	String userInput = scanner.nextLine(); //사용자의 입력을 읽음.
-    	userInput = "HELLO TAEHUN?"; // 이전의 입력을 무시하고 임시로 입력 ㅋㅋ 태훈 ㅎㅇ
     	System.out.println("당신이 입력한 것은" + userInput + "입니다");
+    	
     	
     	scanner.close();
     }
