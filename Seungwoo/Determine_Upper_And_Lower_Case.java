468dbbb7e67b0e4b57fe19827f18212252c49625
Dustin-08 <cjhjik1@gmail.com>
2023-09-21 15:57:00 +0900

    대소문자 판별 내용추가

--- Seungwoo/Determine_Upper_And_Lower_Case.java
+++ Seungwoo/Determine_Upper_And_Lower_Case.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Determine_Upper_And_Lower_Case {
     public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		char value = in.next().charAt(0);
+		String str = in.next();
+
+		char value = str.charAt(0);
 
 		if(Character.isUpperCase(value)){
 			System.out.println("대문자");
 		}else{
-			System.out.println("소문자");
+			if(Character.isLowerCase(value)){
+				System.out.println("소문자");
+			}else{
+				System.out.println("대소문자가 아님");
+		}
 		}
         in.close();
 	}
