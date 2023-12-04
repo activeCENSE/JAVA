adaa66029c6491bc1bcd08ab26bfdd5e17458f43
taehun2123 <pyoneng_@naver.com>
2023-12-04 11:04:37 +0900

    BufferedReader, BufferedWriter

--- taehun/JavaPractice01/study/study/Code1204_2.java
+++ taehun/JavaPractice01/study/study/Code1204_2.java
@@ -1,11 +1,29 @@
 package JavaPractice01.study.study;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Code1204_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int su = Integer.parseInt(br.readLine());
+        int[] data = new int[su];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int i = 0;
+        while(st.hasMoreTokens()){
+            data[i] = Integer.parseInt(st.nextToken());
+            i++;
+        }
+
+        for(int temp:data){
+            bw.write(temp+" ");
+        }
+        bw.write("\n");
+        bw.flush();
+        bw.close();
     }
 }
